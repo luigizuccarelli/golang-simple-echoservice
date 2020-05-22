8e00d1c009d8ebc56b02ff38c960cf089c6311e1
Luigi Zuccarelli <luigizuccarelli@gmail.com>
2020-05-22 16:35:19 +0100

    Updated excludes for sonaqube

--- gocd-cicd.sh
+++ gocd-cicd.sh
@@ -5,8 +5,8 @@
 # declare some variables
 PROJECT="servisbot-authinterface"
 jobname="kaniko-servisbot-authinterface"
-deploymentconfig="authinterface"
-namespace="servisbot"
+deploymentconfig="servisbot-authinterface"
+namespace="trackmate-pilot"
 
 
 # some variable checks
@@ -24,19 +24,26 @@ fi
 
 if [ -z ${OC_USERNAME} ]; 
 then
-  echo -e "\033[0;91mOC_TOKEN envar is not set please set it in the environments tab (secure envar) in GOCD\033[0m"
+  echo -e "\033[0;91mOC_USERNAME envar is not set please set it in the environments tab (secure envar) in GOCD\033[0m"
   exit -1
+fi
+
+if [ -z ${SONAR_TOKEN} ];
+then
+  SONAR_LOGIN="-Dsonar.host.url=${SONARQUBE_HOST} -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASSWORD}"
+else
+  SONAR_LOGIN="-Dsonar.host.url=${SONARQUBE_HOST} -Dsonar.login=${SONAR_TOKEN}"
 fi
 
 # list some gocd variables
 echo -e " "
-echo "GOCD job name          ${GO_JOB_NAME}"
-echo "GOCD pipeline name     ${GO_PIPELINE_NAME}"
-echo "GOCD pipeline counter  ${GO_PIPELINE_COUNTER}"
-echo "GOCD pipeline label    ${GO_PIPELINE_LABEL}"
-echo "GOCD Project           ${PROJECT}"
-echo "GOCD Sonarqube host    ${SONARQUBE_HOST}"
-echo "GOCD Sonarqube scanner ${SONARQUBE_SCANNER_PATH}"
+echo "PROJECT                 ${PROJECT}"
+echo "SONARQUBE_HOST          ${SONARQUBE_HOST}"
+echo "SONARQUBE_SCANNER_PATH  ${SONARQUBE_SCANNER_PATH}"
+echo "GOCD job name           ${GO_JOB_NAME}"
+echo "GOCD pipeline name      ${GO_PIPELINE_NAME}"
+echo "GOCD pipeline counter   ${GO_PIPELINE_COUNTER}"
+echo "GOCD pipeline label     ${GO_PIPELINE_LABEL}"
 echo -e " " 
 
 if [ "$1" = "sonarqube" ]
@@ -46,9 +53,9 @@ then
    touch output.json
    fs=$(stat --printf='%s\n' output.json)
    result="\"PENDING\""
-   ${SONARQUBE_SCANNER_PATH}bin/sonar-scanner -Dsonar.projectKey=${PROJECT} -Dsonar.sources=. -Dsonar.host.url=${SONARQUBE_HOST} -Dsonar.login=${SONARQUBE_USER} -Dsonar.password=${SONARQUBE_PASSWORD} -Dsonar.go.coverage.reportPaths=tests/results/cover.out -Dsonar.exclusions=vendor/**,*_test.go,main.go,connectors.go,schema.go,swaggerui/**,tests/**,*.json,*.txt,*.yml,*.xml,*.sh,Dockerfile -Dsonar.issuesReport.json.enable=true -Dsonar.report.export.path=sonar-report.json -Dsonar.issuesReport.console.enable=true | tee response.txt
+   ${SONARQUBE_SCANNER_PATH}bin/sonar-scanner -Dsonar.projectKey=${PROJECT} ${SONAR_LOGIN} -Dsonar.go.coverage.reportPaths=tests/results/cover.out -Dsonar.exclusions=vendor/**,cmd/**,pkg/connectors/**,pkg/schema/**,pkg/handlers/*_test.go,pkg/validator/*_test.go,swaggerui/**,tests/**,*.json,*.txt,*.yml,*.xml,*.sh,Dockerfile -Dsonar.issuesReport.json.enable=true -Dsonar.report.export.path=sonar-report.json -Dsonar.issuesReport.console.enable=true | tee response.txt
    # response text includes the url to view the json payload of the sonar scanner results
-   url=$(cat response.txt | grep -o "${SONARQUBE_HOST}/api/ce/task?id=[-_A-Za-z0-9]*")
+   url=$(cat response.txt | grep -o "${SONARQUBE_HOST}/api/ce/task?id=[A-Za-z0-9\-]*")
    # loop until we have a valid payload
    while [[ ${fs} -eq 0 ]] && [[ "${result}" = "\"PENDING\"" ]];
    do
@@ -85,7 +92,7 @@ then
  	  exit -1
   else
     echo "Passed"
-    # if we aren't deploying then just exit
+    # if we arent deploying then just exit
     if [ "${AUTODEPLOY}" == "false" ];
     then
       oc delete job/"${jobname}"
@@ -96,21 +103,12 @@ then
   # delete the job
   oc delete job/"${jobname}" 
 
-  # This only applies to a UAT server
-  # We use a different workflow for DEV
-  # For PROD we use a manual (Release Manager) strategy
   if [ "${AUTODEPLOY}" == "true" ];
   then
     # we assume that the project resides on the same server (master-url)
     # if not then add a new login call here first
-    oc login ${UAT_MASTER_URL} --username=${OC_USER} --password=${OC_PASSWORD} --insecure-skip-tls-verify -n ${namespace}
+    oc project ${namespace}
     oc rollout latest dc/${deploymentconfig}
-    # use oc rollout status to check your deplyment
-    # its excluded so tha the cicd process can end in a timely manner
     exit 0
   fi
 fi
-
-
-
-
